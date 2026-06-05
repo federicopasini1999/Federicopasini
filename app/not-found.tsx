bd67a8993431df6f055a8e2b9b80e49af107542a
federicopasini1999 <federico@pasinicreative.com>
2026-06-05 10:23:34 +0200

    Delete app directory

--- app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-black text-white">
-      <h1 className="text-4xl font-bold">Page Not Found</h1>
-    </div>
-  );
-}
